c4b5c328e8060acc1d6da5e1b840b8ce3e3af48b
Tom Turner <tom@twt.me.uk>
2020-12-16 16:39:21 +0000

    Create setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,56 @@
-import setuptools
+ #!/usr/bin/env python
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+import io
+from os.path import exists
 
-setuptools.setup(
-    name="django-filtered-datatables",
-    version="0.0.3",
-    author="Ian Jones",
-    description="Django views with javascript filters using Datatables",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jonesim/django-datatables",
-    include_package_data = True,
-    packages=['django_datatables'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+__version__ = "0.1.0"
+
+setup(
+    name='django_datatables',
+    version=__version__,
+    author='Ian Jones',
+    packages=[
+        'django_datatables',
+        'django_datatables.static',
+        'django_datatables.static.datatables',
+        'django_datatables.static.datatables.css',
+        'django_datatables.static.datatables.js',
+        'django_datatables.templates',
+        'django_datatables.templates.datatables',
+        'django_datatables.templates.datatables.filter_blocks',
+        'django_datatables.templates.datatables.filter_rows',
+        'django_datatables.templates.datatables.plugins',
+        'django_datatables.templatetags',
     ],
-    python_requires='>=3.6',
+    include_package_data=True,
+    scripts=[],
+    url='https://github.com/jonesim/django-datatables',
+    license='MIT',
+    description='Datatables support for Django.',
+    long_description=io.open('README.rst', encoding='utf-8').read() if exists("README.rst") else "",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    # install_requires=[
+    #     'Django >= 2.1,<3.2',
+    # ],
+    zip_safe=False,
 )
